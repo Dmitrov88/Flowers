eade1a0f31aa276b66dd478dd139b9e837405539
Dmitrov88 <39922313+Dmitrov88@users.noreply.github.com>
2018-06-06 14:34:57 +0300

    Add files via upload

--- src/by/htp/flowers/entity/Bocuete.java
+++ src/by/htp/flowers/entity/Bocuete.java
@@ -2,4 +2,22 @@ package by.htp.flowers.entity;
 
 public class Bocuete {
 
+	private Flower[] flowers ;
+	
+	public Bocuete(Flower[] flowers) {
+		this.flowers = flowers;
+	}
+	
+	public int getPrice( ) {
+		int price = 0;
+		
+		for (Flower flower:this.flowers) {
+			price += flower.getPrice();
+			
+		}
+					
+		return price;
+		
+	}
+	
 }
